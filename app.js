9301e70658465ff711c95e25a555a563e04fa038
Aleksandra Kazarinova <fufka93@gmail.com>
2023-02-17 09:56:40 +0200

    fixed server and client  and add nodemon, refactoring code

--- app.js
+++ app.js
@@ -1,55 +1,45 @@
-const express = require('express')
-const path = require('path')
-const {v4}= require('uuid')
-const app = express()
-
+const express = require('express');
+const path = require('path');
+const {v4}= require('uuid');
+const app = express();
 
 let CONTACTS = [
-    {
-    id: 1, name: 'Oleksandra', value: '+38-050-901-21-70', marked: false
-}
-]
+    {id: 1, name: 'Oleksandra', value: '+38-050-901-21-70', marked: false}
+];
 
-app.use(express.json())
+app.use(express.json());
 
 //GET
 app.get('/api/contacts', (req, res) => {
     setTimeout(() => {
-        res.status(200).json(CONTACTS)
-    }, 1000)
-
-})
+        res.status(200).json(CONTACTS);
+    }, 1000);
+});
 
 //POST
 app.post('/api/contacts', (req, res) => {
-    const contact = {...req.body, id: v4(), marked: false}
-    CONTACTS.push(contact)
-    res.status(201).json(contact)
-
-})
+    const contact = {...req.body, id: v4(), marked: false};
+    CONTACTS.push(contact);
+    res.status(201).json(contact);
+});
 
 //DELETE
 app.delete('/api/contacts/:id', (req, res) => {
-    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
-    res.status(200).json({message: 'контакт был удален'})
-})
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id);
+    res.status(200).json({message: 'контакт был удален'});
+});
 
 //PUT
 app.put('/api/contacts/:id', (req, res) => {
-    const inx = CONTACTS.findIndex(c => c.id === req.params.id);
-    CONTACTS[inx] = req.body
-    res.json(CONTACTS[inx])
-})
+    const inx = CONTACTS.findIndex(c => c.id === +req.params.id);
+    CONTACTS[inx] = req.body;
+    res.json(CONTACTS[inx]);
+});
 
-
-
-
-
-
-app.use(express.static(path.resolve(__dirname, 'client')))
+app.use(express.static(path.resolve(__dirname, 'client')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'index.html' ))
-})
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html' ));
+});
 
-app.listen(3000, () => console.log('server has been started on port 3000 ... '))
+app.listen(3000, () => console.log('server has been started on port 3000 ... '));
